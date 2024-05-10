7f507ccbc61eaf32229976e5ab419727a21363ed
wouter <wouterwijkhuizen@gmail.com>
2024-05-10 14:23:50 +0200

    first player

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,69 @@
+use std::{error::Error, io};
+
+use crossterm::{
+    event::{self, DisableMouseCapture, EnableMouseCapture, Event, KeyCode, KeyEventKind},
+    execute,
+    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
+};
+use ratatui::{
+    backend::{Backend, CrosstermBackend},
+    Terminal,
+};
+
 mod ui;
+use crate::ui::{ui, Player};
 
-use crate::ui;
+fn main() -> Result<(), Box<dyn Error>> {
+    enable_raw_mode()?;
 
-fn main() {
-    println!("Hello, world!");
+    let mut stderr = io::stderr();
+    execute!(stderr, EnterAlternateScreen, EnableMouseCapture)?;
+
+    let backend = CrosstermBackend::new(stderr);
+    let mut terminal = Terminal::new(backend)?;
+    let mut player = Player { x: 12.0, y: 12.0 };
+
+    let _res = run_app(&mut terminal, &mut player);
+    disable_raw_mode()?;
+
+    execute!(
+        terminal.backend_mut(),
+        LeaveAlternateScreen,
+        DisableMouseCapture,
+    )?;
+
+    terminal.show_cursor()?;
+
+    Ok(())
+}
+
+fn run_app<B: Backend>(terminal: &mut Terminal<B>, player: &mut Player) -> io::Result<bool> {
+    loop {
+        terminal.draw(|f| ui(f, player))?;
+
+        if let Event::Key(key) = event::read()? {
+            if key.kind == event::KeyEventKind::Release {
+                // Skip events that are not KeyEventKind::Press
+                continue;
+            }
+            match key.code {
+                KeyCode::Char('q') => {
+                    return Ok(true);
+                }
+                KeyCode::Right => {
+                    player.x += 1.0;
+                }
+                KeyCode::Left => {
+                    player.x -= 1.0;
+                }
+                KeyCode::Up => {
+                    player.y += 1.0;
+                }
+                KeyCode::Down => {
+                    player.y -= 1.0;
+                }
+                _ => {}
+            }
+        }
+    }
 }
--- src/ui.rs
+++ src/ui.rs
@@ -5,11 +5,16 @@ use ratatui::{
     widgets::{canvas::*, Block, Borders, Clear, List, ListItem, Paragraph, Wrap},
     Frame,
 };
+#[derive(Debug)]
+pub struct Player {
+    pub x: f64,
+    pub y: f64,
+}
 
-pub fn ui(f: &mut Frame) {
+pub fn ui(f: &mut Frame, player: &Player) {
     let chunks = Layout::default()
         .direction(Direction::Vertical)
-        .constrains([
+        .constraints([
             Constraint::Length(2),
             Constraint::Min(1),
             Constraint::Length(5),
@@ -25,9 +30,30 @@ pub fn ui(f: &mut Frame) {
 
     f.render_widget(title, chunks[0]);
 
+    let screen = Canvas::default()
+        .block(Block::bordered().title("World"))
+        .paint(|ctx| {
+            ctx.draw(&Circle {
+                x: player.x,
+                y: player.y,
+                radius: 5.0,
+                color: Color::Red,
+            })
+        })
+        .x_bounds([-180.0, 180.0])
+        .y_bounds([-90.0, 90.0]);
+    f.render_widget(screen, chunks[1]);
+
     let ground = Canvas::default()
-        .background_color(Color::White)
-        .block(Block::new());
+        .block(Block::bordered().title("World"))
+        .paint(|ctx| {
+            ctx.draw(&Map {
+                color: Color::Green,
+                resolution: MapResolution::High,
+            });
+        })
+        .x_bounds([-180.0, 180.0])
+        .y_bounds([-90.0, 90.0]);
 
     f.render_widget(ground, chunks[2]);
 }
